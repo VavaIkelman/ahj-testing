c877738e94b460bc33c66ac056f80d9755af2dfb
VavaIkelman <mr.ilkevich@gmail.com>
2021-06-29 18:39:05 +0300

    Init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './css/style.css';
+
+import './js/app';
+
+import './img/american-express.png';
+import './img/diners-club.png';
+import './img/discover.png';
+import './img/jcb.png';
+import './img/maestro.png';
+import './img/mastercard.png';
+import './img/mir.png';
+import './img/unionpay.png';
+import './img/visa.png';
+
+import './licenses.txt';
