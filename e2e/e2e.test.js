e87404ddf98b1829d519de9c9a966591da566294
VavaIkelman <mr.ilkevich@gmail.com>
2021-09-16 19:07:42 +0200

    Modified e2e.server.js

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,32 +1,39 @@
 import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
-describe('INN/OGRN form', () => {
+describe('Credit Card Validator form', () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 500,
-      devtools: true, // show devTools
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
 
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
 
-  test('should add .valid class for valid inn', async () => {
+  test('should add do something', async () => {
     await page.goto(baseUrl);
-    const form = await page.$('[data-widget=innogrn-form-widget]');
-    const input = await form.$('[data-id=innogrn-input]');
-    await input.type('7715964180');
-    const submit = await form.$('[data-id=innogrn-submit]');
-    submit.click();
-    await page.waitFor('[data-id=innogrn-input].valid');
   });
 });
